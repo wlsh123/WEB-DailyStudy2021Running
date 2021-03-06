b595db8b48e5f67e202e4420ae35f4569d8bb020
wanglsh <975135062@qq.com>
2021-03-06 18:12:48 +0800

    新增js分支

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # WEB-DailyStudy2021Running
 记录每天的学习情况，争取2021年的10月转行前端
+
+# 配置ignore文件地址
+https://github.com/github/gitignore
--- react/day01/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-//import App from './App';
-import App2 from './App2';
-
-ReactDOM.render(
-    <App2></App2>,
-    document.getElementById("root")
-)
