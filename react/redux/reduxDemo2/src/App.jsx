073752801715ac33150361105fa38f38f46f0596
良升 <975135062@qq.com>
2021-04-05 22:24:24 +0800

    增加action和constant文件

---
+++ react/redux/reduxDemo2/src/App.jsx
@@ -0,0 +1,12 @@
+
+import React, { Component } from 'react';
+import Count from './components/count';
+class App extends Component {
+  render() { 
+    return ( 
+      <Count/>
+     );
+  }
+}
+ 
+export default App;
