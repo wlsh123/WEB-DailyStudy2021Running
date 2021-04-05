167e7f7d6ecf05eba8b6cf4bcb3aed2bb1d3ce0f
良升 <975135062@qq.com>
2021-04-05 21:44:58 +0800

    redux精简版Demo

--- react/redux/reduxDemo1/src/components/count/index.jsx
+++ react/redux/reduxDemo1/src/components/count/index.jsx
@@ -1,35 +1,43 @@
 import React, { Component } from 'react';
+//引入store，用于获取状态
+import store from '../../redux/store'
 class Count extends Component {
-  state = {count:0}
+  // state = {count:0}
+  /*componentDidMount(){
+    //监测redux中状态的变化，只要变化，就调用render
+    store.subscribe(()=>{
+      // console.log('@@');
+      this.setState({})
+    })这个存在效率问题，放在入口文件中。只调用一次render
+  }
+  */
   increment = ()=>{
     const {value} = this.selectNumber
-    const {count} = this.state
-    this.setState({count:count+value*1})
+    store.dispatch({ type:'increment', data:value*1})
   }
   decrement = ()=>{
     const { value } = this.selectNumber
-    const { count } = this.state
-    this.setState({ count: count - value * 1 })
+    store.dispatch({ type: 'decrement', data: value * 1 })
   }
   incrementIfOdd = ()=>{
     const { value } = this.selectNumber
-    const { count } = this.state
+    const count = store.getState()
     if (count % 2 !== 0) {
-      this.setState({count:count+value*1})
+      store.dispatch({type:'increment', data:value*1})
     }
   }
   incrementAsync = ()=>{
     const { value } = this.selectNumber
-    const { count } = this.state
+    const count = this.state
     setTimeout(() => {
-      this.setState({ count: count + value * 1 })
+      store.dispatch({ type: 'increment', data: value * 1 })
     }, 500);
   }
   render() { 
-    console.log(this)
+    // console.log(this)
     return ( 
       <div>
-        <h1>当前求和为：{this.state.count}</h1>
+        <h1>当前求和为：{store.getState()}</h1>
         <select ref={c => { this.selectNumber = c }}>
           <option value="1">1</option>
           <option value="2">2</option>
--- react/redux/reduxDemo1/src/index.jsx
+++ react/redux/reduxDemo1/src/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import store from "./redux/store";
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+store.subscribe(()=>{
+  ReactDOM.render(<App />, document.getElementById('root'))
+})
